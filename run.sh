e86c15bd0447aabb2ef7044da34f35f10bbcdda7
Oleksii Chusta <alexey.chusta@gmail.com>
2020-10-09 14:51:04 +0300

    Add nginx http proxy

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker stop simple-php-app
-docker rm simple-php-app
+docker stop simple-docker-php
+docker rm simple-docker-php
 docker rmi alexeychusta/simple-php-app
-docker run -d --name simple-php-app -p 1234:80 alexeychusta/simple-php-app
+docker run -d --name simple-docker-php -p 1234:80 alexeychusta/simple-php-app
